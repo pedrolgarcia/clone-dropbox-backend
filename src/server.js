ea700aa0bbee53e911f3f8be567462d5f4160317
Pedro Lopes <pedro@sixcreative.com.br>
2019-04-20 16:15:30 -0300

    add variáveis ambiente

--- src/server.js
+++ src/server.js
@@ -30,4 +30,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(3333, () => console.log('Running on port 3333...'));
+app.listen(process.env.PORT || 3333, () => console.log('Running on port 3333...'));
